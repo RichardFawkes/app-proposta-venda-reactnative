d9c225ebe391e5aca21b2c7b3c1922d0dbc37fb2
Richard <richard.@Richards-MacBook-Pro.local>
2023-01-21 23:43:58 -0300

    add ts relatorio

--- src/pages/reportsProposals/index.tsx
+++ src/pages/reportsProposals/index.tsx
@@ -5,16 +5,34 @@ import { PieChart } from 'react-native-svg-charts';
 import { db, collection, query, onSnapshot } from '../../config/firebase';
 import { Text } from 'react-native-svg';
 export default function ReportProposals() {
-  const q = query(collection(db, 'propostas'));
-  const [Proposal, setProposal] = useState();
+  type Proposal = {
+    id: string;
+    proposal_status: boolean;
+    user_cidade: string;
+    user_cpf: number;
+    user_estado: string;
+    user_lastname: string;
+    user_name: string;
+    user_status: boolean;
+    user_updateAt: string;
+  };
+
+  const tableDb = query(collection(db, 'propostas'));
+  const [proposals, setProposals] = useState<Proposal[]>([]);
   const [counterSp, setCounterSp] = useState();
   const [counterRj, setCounterRj] = useState();
   const [counterMg, setCounterMg] = useState();
-  onSnapshot(q, (querySnapshot) => {
-    const list = [];
+  onSnapshot(tableDb, (querySnapshot) => {
+    const list: Proposal[] = [];
+
     querySnapshot.forEach((docSnapshot) => {
-      list.push({ ...docSnapshot.data(), id: docSnapshot.id });
+      list.push({
+        id: docSnapshot.id,
+        ...(docSnapshot.data() as Proposal),
+      });
     });
+    console.log('lista', list);
+
     let counter_sp = 0;
     let counter_rj = 0;
     let counter_mg = 0;
@@ -37,7 +55,7 @@ export default function ReportProposals() {
     );
 
   const pieData = data
-    .filter((value) => value > 0)
+    .filter((value) => (value as any) > 0)
     .map((value, index) => ({
       value,
       svg: {
@@ -46,29 +64,42 @@ export default function ReportProposals() {
       key: `pie-${index}`,
     }));
 
-  const Label = ({ slices }) => {
-    return slices.map((slice, index) => {
-      const { pieCentroid, data } = slice;
-      return (
-        <Text
-          key={`label-${index}`}
-          x={pieCentroid[0]}
-          y={pieCentroid[1]}
-          fill="white"
-          textAnchor={'middle'}
-          alignmentBaseline={'middle'}
-          fontSize={15}
-        >
-          {data.value} %
-        </Text>
-      );
-    });
+  interface Slice {
+    pieCentroid: [number, number];
+    data: {
+      value: number;
+    };
+  }
+
+  const Label = ({ slices }: { slices: Slice[] }) => {
+    return slices && slices.length > 0 ? (
+      <React.Fragment>
+        {slices.map((slice, index) => {
+          const { pieCentroid, data } = slice;
+          return (
+            <Text
+              key={`label-${index}`}
+              x={pieCentroid[0]}
+              y={pieCentroid[1]}
+              fill="white"
+              textAnchor={'middle'}
+              alignmentBaseline={'middle'}
+              fontSize={15}
+            >
+              {data.value} %
+            </Text>
+          );
+        })}
+      </React.Fragment>
+    ) : (
+      <Text>No slices data found</Text>
+    );
   };
 
   return (
     <View>
       <PieChart style={{ height: 200 }} data={pieData}>
-        <Label slices={undefined} />
+        <Label slices={[]} />
       </PieChart>
     </View>
   );
